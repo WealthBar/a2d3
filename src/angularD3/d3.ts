97b885ec34b9747a31c5900bf9681a829bee386e
Chris Nicola <chris.nicola@wealthbar.com>
2016-09-01 14:39:56 -0700

    Switch from typings to @types provided by npm

---
+++ src/angularD3/d3.ts
@@ -0,0 +1,31 @@
+import { selection } from "d3-selection";
+import { scales } from "d3-scale";
+let format = require("d3-format");
+let interpolate = require("d3-interpolate");
+
+export default {
+  arc: shape.arc,
+  area: shape.area,
+  brush: brush.brush,
+  brushX: brush.brushX,
+  brushY: brush.brushY,
+  extent: array.extent,
+  format: format.format,
+  interpolate: interpolate.interpolate,
+  line: shape.line,
+  max: array.max,
+  min: array.min,
+  mouse: selection.mouse,
+  pie: shape.pie,
+  range: array.range,
+  rgb: color.rgb,
+  select: selection.select,
+  selectAll: selection.selectAll,
+  scaleBand: scales.scaleBand,
+  scaleLinear: scales.scaleLinear,
+  scaleOrdinal: scales.scaleOrdinal,
+  scalePoint: scales.scalePoint,
+  scaleQuantile: scales.scaleQuantile,
+  scaleTime: scales.scaleTime,
+  treemap: shape.treemap
+};
