377cbed472f5ab21d1991a22b4245ef6253c89ce
Jean-Francois Arseneau <jf.arseneau@gmail.com>
2017-03-20 17:54:32 -0700

    Remove spec-bundle

--- karma.conf.js
+++ karma.conf.js
@@ -26,8 +26,6 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      // 'spec-bundle.js': ['browserify']
-      // 'spec-bundle.js': ['webpack', 'sourcemap']
       'test/**/*.spec.ts': ['webpack']
     },
 
